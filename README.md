b2e201d799b124899851be38fefc11e8e6b75d3c
aaryan144 <74712377+aaryan144@users.noreply.github.com>
2020-11-23 16:40:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C-20
-Collision detection algorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect = createSprite(500, 200, 50, 80);
+  movingrect = createSprite(400, 200, 80, 30);
+
+  fixedrect.shapeColor = "green";
+  movingrect.shapeColor = "green";
+}
+
+function draw() {
+  background("lightblue");
+
+  movingrect.x = World.mouseX;
+  movingrect.y = World.mouseY;
+
+
+  if(movingrect.x - fixedrect.x <
+    movingrect.width/2 + fixedrect.width/2 &&
+    fixedrect.x - movingrect.x <
+    movingrect.width/2 + fixedrect.width/2 &&
+    movingrect.y - fixedrect.y <
+    movingrect.height/2 + fixedrect.height/2 &&
+    fixedrect.y - movingrect.y <
+    movingrect.height/2 + fixedrect.height/2){
+
+      fixedrect.shapeColor = "red";
+      movingrect.shapeColor = "red";
+
+    }
+  else {
+    
+    fixedrect.shapeColor = "green";
+    movingrect.shapeColor = "green";
+        
+  }
+  drawSprites();
+}
